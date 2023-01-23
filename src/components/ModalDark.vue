acd769d527f4d7daeffe34e94dd214189fecd5dd
Bohdan Leonets <bohdan@mytowerapp.com>
2023-01-23 17:52:41 +0200

    Dynamic Components

---
+++ src/components/ModalDark.vue
@@ -0,0 +1,46 @@
+<template>
+  <teleport to=".modals-container">
+    <div v-if="modelValue" class="modal-dark">
+      <h1>{{ title }}</h1>
+
+      <slot />
+
+      <button @click="handleButtonClick">Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: 'No title specified',
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const handleButtonClick = () => {
+  emit('update:modelValue', false);
+};
+</script>
+
+<style>
+.modal-dark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background: #333;
+  color: #fff;
+  padding: 10px;
+}
+</style>
+
+
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,20 +2,30 @@
   <div class="modals">
     <h1>modals-view</h1>
 
+    <div>
+      <label>
+        Show dark modals?
+        <input v-model="showDarkModals" type="checkbox">
+      </label>
+    </div>
+
     <button @click="showModal = true">Show modal</button>
 
-    <modal
+    <component
+      :is="showDarkModals ? ModalDark : Modal"
       v-model="showModal"
       title="This is modal"
     >
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut commodi illo labore, reiciendis sit temporibus! Animi aperiam consequuntur dolores earum quasi repudiandae sequi, suscipit voluptatem! Assumenda labore nisi sit!</p>
-    </modal>
+    </component>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import Modal from '@/components/Modal.vue';
+import ModalDark from '@/components/ModalDark.vue';
 
+const showDarkModals = ref(false);
 const showModal = ref(false);
 </script>
