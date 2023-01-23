76830dde707ea263694b56e831292b02a6637703
Bohdan Leonets <bohdan@mytowerapp.com>
2023-01-23 17:05:25 +0200

    Child Components, Lazy-Loading Views

---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is modal</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut commodi illo labore, reiciendis sit temporibus! Animi aperiam consequuntur dolores earum quasi repudiandae sequi, suscipit voluptatem! Assumenda labore nisi sit!</p>
+
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showModal = ref(false);
+</script>
+
+<style>
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background: beige;
+  padding: 10px;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -4,33 +4,13 @@
 
     <button @click="showModal = true">Show modal</button>
 
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is modal</h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut commodi illo labore, reiciendis sit temporibus! Animi aperiam consequuntur dolores earum quasi repudiandae sequi, suscipit voluptatem! Assumenda labore nisi sit!</p>
-
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <modal v-if="showModal" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 
 const showModal = ref(false);
 </script>
-
-<style>
-.modal {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-  background: beige;
-  padding: 10px;
-}
-</style>
